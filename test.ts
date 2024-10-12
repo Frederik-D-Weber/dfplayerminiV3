da8d21b44444ecf93e695cf390e3fdc85bbd38ba
Frederik-D-Weber <Frederik-D-Weber@users.noreply.github.com>
2024-10-12 11:29:12 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -8,7 +8,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     dfplayermini.playLoopFolder(1)
 })
-dfplayermini.connect(SerialPin.P0, SerialPin.P1)
+dfplayermini.connect(SerialPin.P0, SerialPin.P1, dfplayermini.isVersion3.No)
 dfplayermini.setVolume(30)
 dfplayermini.press(dfplayermini.playType.Play)
 dfplayermini.playLoopAllFiles()
